c65a088343f2dd8474dc9fb05df824bd38421561
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2024-06-25 07:47:13 +0900

    Remove build folder and script.

--- bun.build.js
+++
@@ -1,9 +0,0 @@
-import html from 'bun-plugin-html';
-
-await Bun.build({
-  entrypoints: ['./public/index.html'],
-  outdir: './build',  // Specify the output directory
-  plugins: [
-    html()
-  ],
-});
